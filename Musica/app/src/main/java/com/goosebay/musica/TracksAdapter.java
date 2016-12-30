436c72c1698c3fc1c7bd85da8aadae0be83fff65
Maribel Romo <mromosav@gmail.com>
2016-12-30 10:01:10 -0500

    - Adding album art to list items using Picasso - Adding new app icon

--- Musica/app/src/main/java/com/goosebay/musica/TracksAdapter.java
+++ Musica/app/src/main/java/com/goosebay/musica/TracksAdapter.java
@@ -6,13 +6,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import kaaes.spotify.webapi.android.models.AlbumSimple;
 import kaaes.spotify.webapi.android.models.ArtistSimple;
+import kaaes.spotify.webapi.android.models.Image;
 import kaaes.spotify.webapi.android.models.Track;
 
 /**
@@ -44,6 +49,27 @@ public class TracksAdapter extends ArrayAdapter<Track> {
 
         if (artists.size() > 0) {
             artistNameView.setText(artists.get(0).name);
+        } else {
+            artistNameView.setText("");
+        }
+
+        // Set the album art
+        AlbumSimple album = track.album;
+        ImageView albumArtImageView =  (ImageView) convertView.findViewById(R.id.albumArt);
+
+        // Set the default album art
+        albumArtImageView.setImageResource(R.drawable.default_album_art);
+
+        if (album != null){
+            List<Image> images = album.images;
+
+            if (images != null && images.size() > 0) {
+                Image firstAlbumArt = album.images.get(0);
+                Picasso.with(getContext()).load(firstAlbumArt.url)
+                        .placeholder(R.drawable.default_album_art)
+                        .error(R.drawable.default_album_art)
+                        .into(albumArtImageView);
+            }
         }
 
         ImageButton addButton = (ImageButton) convertView.findViewById(R.id.addButton);
@@ -52,12 +78,15 @@ public class TracksAdapter extends ArrayAdapter<Track> {
             @Override
             public void onClick(View view) {
                 int position = (Integer)view.getTag();
-                SpotifyManager.getInstance().addToSavedTracks(getItem(position), new SpotifyManager.CompleteListener<Object>() {
-                    public void onComplete(Object o){
-                        Toast.makeText(getContext(),"Track was added.",Toast.LENGTH_SHORT).show();
-                    }
-                    public void onError(Throwable error){
-                        Toast.makeText(getContext(),"Unable to add track.",Toast.LENGTH_SHORT).show();
+                SpotifyManager.getInstance().addToSavedTracks(getItem(position),
+                    new SpotifyManager.CompleteListener<Object>() {
+                        public void onComplete(Object o){
+                            Toast.makeText(getContext(),"Track was added.",Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                        public void onError(Throwable error){
+                            Toast.makeText(getContext(),"Unable to add track.",Toast.LENGTH_SHORT)
+                                    .show();
                     }
                 });
             }
